705970fdc66d27ac02a8635a141d54dc73cf66e2
zakumann <samuel742able@gmail.com>
2023-06-28 22:27:47 +0900

    Mapping Arrays to Elements

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -8,18 +8,29 @@ class App extends Component{
     super();
 
     this.state = {
-      name: 'Andrei',
-    }
+      monsters: [
+        {
+          name: 'Linda',
+        },
+        {
+          name: 'Frank',
+        },
+        {
+          name: 'Jacky',
+        },
+        {
+          name: 'Andrei',
+        },
+      ],
+    };
   }
 
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name}</p>
-          <button>Change Name</button>
-        </header>
+        {this.state.monsters.map((monster) => {
+          return <h1>{monster.name}</h1>;
+        })}
       </div>
     );
   }
