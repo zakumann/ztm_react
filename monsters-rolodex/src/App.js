afc83a9fadb3cbd1d4d571961eb25fad2c8164b8
zakumann <samuel742able@gmail.com>
2023-08-06 22:46:05 +0900

    August sixth 2023

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -30,12 +30,22 @@ class App extends Component{
     );
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  }
+
   render(){
     console.log('render');
 
-    const filteredMonsters = this.state.monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
-    });
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    })
 
     return (
       <div className="App">
@@ -43,13 +53,7 @@ class App extends Component{
           className = 'search-box'
           type='search'
           placeholder='search monsters'
-          onChange={(event) => {
-            const searchField = event.target.value.toLocaleLowerCase();
-            this.setState(
-            () => {
-              return { searchField }
-            });
-          }}
+          onChange={onSearchChange}
         />
         {filteredMonsters.map((monster) => {
           return (
